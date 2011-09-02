6194bb28362d5f33ee5dc97a966a45246ca3fa92
Avi Tzurel <avi@kensodev.com>
2011-09-02 23:12:49 +0300

    Initial commit of a rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PerformLaterSampleProject::Application.config.secret_token = '13d99ea9ba67e328da4f1235b21155fc02c221b0942cfe64421ee115ee2b5b2040ed74d54f4cf088dcced7b42e436cd5f4e72e6b44e402e7853cd8819a4226f4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PerformLaterSampleProject::Application.config.session_store :cookie_store, :key => '_perform_later_sample_project_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PerformLaterSampleProject::Application.config.session_store :active_record_store
